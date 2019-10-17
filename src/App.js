2bd70f6845f25f6e88ae10701d8d20b5bae45d7a
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-17 18:17:03 -0400

    Code cleanup.

--- src/App.js
+++ src/App.js
@@ -11,44 +11,50 @@ import {
 
 const content = {};
 function importAll (r) {
-  r.keys().forEach(key => content[key] = r(key));
+  r.keys().forEach(key => {
+    const route = key.replace(/^\.+/, '').replace(/\.mdx$/, '');
+    const contentModule = r(key);
+    content[route] = {
+      route,
+      Component: contentModule.default
+    };
+  });
 }
 importAll(require.context('!babel-loader!mdx-loader!./content/', true, /\.mdx$/));
 
-function domToText(element) {
-  let text = '';
+// TODO: use htmlparser2 and build the index offline during production builds. Only build the index in the browser on dev builds.
+if (process.env.NODE_ENV !== 'production') {
+  function domToText(element) {
+    let text = '';
 
-  for (let i = 0, len = element.childNodes.length; i < len; i++) {
-    let child = element.childNodes[i];
-    if (child.nodeType === 3) {
-      // Text nodes
-      text += ' ' + child.nodeValue;
+    for (let i = 0, len = element.childNodes.length; i < len; i++) {
+      let child = element.childNodes[i];
+      if (child.nodeType === 3) {
+        // Text nodes
+        text += ' ' + child.nodeValue;
+      }
+      if (child.childNodes.length > 0) {
+        // Parent nodes
+        text += domToText(child);
+      }
     }
-    if (child.childNodes.length > 0) {
-      // Parent nodes
-      text += domToText(child);
-    }
+
+    return text.replace(/\s+/g, ' ');
   }
 
-  return text.replace(/\s+/g, ' ');
-}
+  const domParser = new DOMParser();
+  function plaintext(ReactComponent)
+  {
+    const element = React.createElement(ReactComponent);
+    const html = ReactDOMServer.renderToString(element);
+    const doc = domParser.parseFromString(html, 'text/html');
+    return domToText(doc);
+  }
 
-const domParser = new DOMParser();
-function plaintext(ReactComponent)
-{
-  const element = React.createElement(ReactComponent);
-  const html = ReactDOMServer.renderToString(element);
-  const doc = domParser.parseFromString(html, 'text/html');
-  return domToText(doc);
-}
-
-const routes = {};
-for (const key of Object.keys(content)) {
-  const route = key.replace(/^\.+/, '').replace(/\.mdx$/, '');
-  const Component = content[key].default;
-  const text = plaintext(Component);
-  routes[route] = { Component, text };
-  console.log(route, text);
+  for (const route of Object.keys(content)) {
+    content[route].text = plaintext(content[route].Component);
+    console.log(route, content[route].text);
+  }
 }
 
 function App() {
@@ -56,7 +62,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          {Object.keys(routes).map(route => <Route exact path={route} key={route}>{routes[route].Component}</Route>)}
+          {Object.keys(content).map(route => <Route exact path={route} key={route}>{content[route].Component}</Route>)}
           <Route path="/">
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
