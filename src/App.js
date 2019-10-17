7f77127332442f4106d25fbd00b40f4ea3dc5855
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-16 23:05:35 -0400

    Load test mdx file.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/* eslint-disable import/no-webpack-loader-syntax */
+import Test from '!babel-loader!mdx-loader!./content/guests/test.mdx';
+
 function App() {
   return (
     <div className="App">
@@ -10,14 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Test/>
       </header>
     </div>
   );
