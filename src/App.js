33e615d086ec35921409ab6e1cca177b756032f2
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-17 07:04:31 -0400

    Add react-router.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const content = {};
 function importAll (r) {
@@ -8,18 +14,30 @@ function importAll (r) {
 }
 importAll(require.context('!babel-loader!mdx-loader!./content/', true, /\.mdx$/));
 
+console.log(Object.keys(content)[0]);
 var Test = content[Object.keys(content)[0]].default;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Test/>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/guests/test">
+            <Test />
+          </Route>
+          <Route path="/">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <p>
+                <Link to="/guests/test">Test link</Link>
+              </p>
+            </header>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
