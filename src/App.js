fc7b27aa2669722a65f1914ca0895ad7386a697f
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-19 17:42:48 -0400

    Scroll on navigate.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import MdxPage from './MdxPage';
+import ScrollToTop from './ScrollToTop';
 import { routes } from './contentFiles';
 import { index } from './searchIndex';
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <ScrollToTop />
         <Switch>
           {Object.keys(routes).map(route => <Route exact path={route} key={route}><MdxPage route={route} /></Route>)}
           <Route path="/">
