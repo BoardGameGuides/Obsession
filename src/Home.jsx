add99493718dc5ba5220f1b9dbf06bb17be84ce8
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-18 23:30:19 -0400

    Remove React logo.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './logo.svg';
 import { routes } from './contentFiles';
 
 function Home() {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
       {Object.keys(routes).map(route => <p key={route}><Link to={route}>{routes[route].metadata.title}</Link></p>)}
     </header>
   );
