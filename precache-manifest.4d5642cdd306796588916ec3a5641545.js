760260c72c0d50be2b6db89e4dba131406506cb4
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-20 16:26:18 -0400

    Updates

---
+++ precache-manifest.4d5642cdd306796588916ec3a5641545.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429381457fc98ae5d3558c08f401fad9",
+    "url": "/Obsession/index.html"
+  },
+  {
+    "revision": "64b90970b3bd4e47c1e5",
+    "url": "/Obsession/static/css/main.b9b28da3.chunk.css"
+  },
+  {
+    "revision": "04bb99889fc3571c20c0",
+    "url": "/Obsession/static/js/2.b54077dc.chunk.js"
+  },
+  {
+    "revision": "64b90970b3bd4e47c1e5",
+    "url": "/Obsession/static/js/main.5730d2f0.chunk.js"
+  },
+  {
+    "revision": "53748e60acbe3a465915",
+    "url": "/Obsession/static/js/runtime-main.08c13e2b.js"
+  },
+  {
+    "revision": "21e69e57d8325a2159d26e434e589291",
+    "url": "/Obsession/static/media/barn.21e69e57.png"
+  },
+  {
+    "revision": "9bb611721397399e455faeb1e4288231",
+    "url": "/Obsession/static/media/sara-forbes-bonetta.9bb61172.jpg"
+  }
+]);
