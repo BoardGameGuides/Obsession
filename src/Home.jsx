0d41a5034d93e640c14f5d5811f770bd6759a09e
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-20 14:06:19 -0400

    Add work-in-progress note.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
@@ -11,6 +12,9 @@ function Home() {
       </p>
       <p>
         For now, you can search the articles using the search box at the top of the page.
+      </p>
+      <p>
+        This is just a proof-of-concept. Only the pages for <Link to="/guest/casual/sara-forbes-bonetta">Sara</Link> and the <Link to="/tile/barn">Barn</Link> have images, and the Barn one is out of date.
       </p>
     </div>
   );
