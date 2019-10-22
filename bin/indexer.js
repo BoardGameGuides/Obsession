40c159dec7fcf9d32da745bce8d20c4ad43e0966
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-21 23:08:18 -0400

    Remove old TODOs.

--- bin/indexer.js
+++ bin/indexer.js
@@ -6,7 +6,6 @@ import { pipelineFunctions, searchPipelineFunctions } from '../src/shared/search
  * @param {{id: string, type: string, text: string, metadata: {}}[]} files The files to index.
  */
 export function buildIndex(files) {
-  // TODO: determine if we want a simplified stop-word filter.
   const builder = new Builder();
   builder.pipeline.add(...pipelineFunctions);
   builder.searchPipeline.add(...searchPipelineFunctions);
@@ -14,12 +13,6 @@ export function buildIndex(files) {
   builder.field('title', { boost: 1.5 });
   builder.field('text');
 
-  // TODO: auto-add fields found in metadata as space-delimited exact-match tokens.
-  // builder.field('type');
-  // builder.field('category');
-  // builder.field('vp'); // <number> - the VP value of the card
-  // builder.field('casual'); // 'true' or 'false' - whether a guest is a casual guest
-  // builder.field('starter'); // 'true' or 'false' - whether a guest is a starter guest
   for (const file of files) {
     const doc = Object.assign(file.metadata, {
       id: file.id,
