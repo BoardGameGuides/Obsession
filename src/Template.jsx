1a2810e4e071e78151b0db6e17a2bbf7237d133e
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-24 19:42:11 -0400

    Bootstrap navbar.

--- src/Template.jsx
+++ src/Template.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Navbar } from 'react-bootstrap';
+import logo30 from './logo30.png';
+import { name } from './shared/game-specific/properties';
 
 /**
  * @param {{ children: React.ReactNode; route: string; }} props
  */
 export default function Template(props) {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <Link to="/search">Search</Link>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <div className="content">
-            {props.children}
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <Navbar bg="light">
+        <Navbar.Brand>
+          <Link to="/">
+            <img src={logo30} width="30" height="30" alt={name + " logo"} />
+          </Link>
+        </Navbar.Brand>
+        <Link to="/search">Search</Link>
+      </Navbar>
+      <Container fluid>
+        <Row>
+          <Col>
+            <div className="content">
+              {props.children}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
