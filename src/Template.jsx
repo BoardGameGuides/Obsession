7d32384cc9526a775a733285fb190e5b3d19e405
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-24 21:46:46 -0400

    Add buttons to toolbar.

--- src/Template.jsx
+++ src/Template.jsx
@@ -1,32 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Row, Col, Navbar } from 'react-bootstrap';
+import { Link, withRouter } from 'react-router-dom';
+import { Container, Row, Col, Navbar, Form, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faWrench, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import logo30 from './logo30.png';
 import { name } from './shared/game-specific/properties';
+
+const isInStandaloneMode = (window.matchMedia('(display-mode: standalone)').matches) || (window.navigator['standalone']) || document.referrer.includes('android-app://');
 
 /**
  * @param {{ children: React.ReactNode; route: string; }} props
  */
-export default function Template(props) {
-  return (
-    <div>
-      <Navbar bg="light">
-        <Navbar.Brand>
-          <Link to="/">
-            <img src={logo30} width="30" height="30" alt={name + " logo"} />
-          </Link>
-        </Navbar.Brand>
-        <Link to="/search">Search</Link>
-      </Navbar>
-      <Container fluid>
-        <Row>
-          <Col>
-            <div className="content">
-              {props.children}
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+class Template extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.back = this.back.bind(this);
+  }
+
+  back() {
+    this.props.history.goBack();
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar bg="light">
+          <Navbar.Brand>
+            <Link to="/">
+              <img src={logo30} width="30" height="30" alt={name + " logo"} />
+            </Link>
+          </Navbar.Brand>
+          {!isInStandaloneMode ? null :
+            <Form inline>
+              <Button onClick={this.back} variant="outline-primary"><FontAwesomeIcon icon={faArrowLeft} /> Back</Button>
+            </Form>
+          }
+          <Form inline>
+            <Button as={Link} to="/search" variant="outline-primary"><FontAwesomeIcon icon={faSearch} /> Search</Button>
+          </Form>
+          <Form inline>
+            <Button as={Link} to="/settings" variant="outline-primary"><FontAwesomeIcon icon={faWrench} /> Settings</Button>
+          </Form>
+        </Navbar>
+        <Container fluid>
+          <Row>
+            <Col>
+              <div className="content">
+                {this.props.children}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
+
+export default withRouter(Template);
