b9966158ce1200856ebda398fc8c4204b2291af9
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-25 19:29:46 -0400

    Updates

---
+++ precache-manifest.387cd7b7c8ee48897d9d8f24c817b6f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac19b39bb35f4d1a7bcc37c64cc5b47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fe3d71b4ae7722292ef",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0238688977204e71690c",
+    "url": "./static/css/main.7145f95e.chunk.css"
+  },
+  {
+    "revision": "3fe3d71b4ae7722292ef",
+    "url": "./static/js/2.65128a31.chunk.js"
+  },
+  {
+    "revision": "0238688977204e71690c",
+    "url": "./static/js/main.1fcd8b57.chunk.js"
+  },
+  {
+    "revision": "f9a113edc8485b5ad61d",
+    "url": "./static/js/runtime-main.8b464e81.js"
+  },
+  {
+    "revision": "21e69e57d8325a2159d26e434e589291",
+    "url": "./static/media/barn.21e69e57.png"
+  },
+  {
+    "revision": "9bb611721397399e455faeb1e4288231",
+    "url": "./static/media/sara-forbes-bonetta.9bb61172.jpg"
+  }
+]);
