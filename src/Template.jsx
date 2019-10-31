0aafea3806e1a41cbaa7bc8f570b7a1506917375
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-10-30 22:40:54 -0400

    Use hooks for Template.

--- src/Template.jsx
+++ src/Template.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Container, Row, Col, Navbar, Form, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faWrench, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
@@ -9,67 +9,51 @@ import { name } from './shared/game-specific/properties';
 import { CurrentRouteContext } from './contentFiles';
 
 /**
- * @typedef {object} Props
- * @prop {string} route
- * @prop {import("history").History} history
- *
- * @extends {React.Component<Props>}
+ * 
+ * @param {{route: string; children?: import('react').ReactNode;}} props 
  */
-class Template extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.back = this.back.bind(this);
-  }
-
-  back() {
-    this.props.history.goBack();
-  }
-
-  render() {
-    const logo = <img src={logo30} width="30" height="30" alt={name + " logo"} />;
-    return (
-      <CurrentRouteContext.Provider value={this.props.route}>
-        <Navbar bg="light">
-          <ul className="navbar-nav mr-auto">
-            <Navbar.Brand>
-              {this.props.route === '/' ? logo :
-                <Link to="/">
-                  {logo}
-                </Link>
-              }
-            </Navbar.Brand>
+export default function Template(props) {
+  const history = useHistory();
+  const logo = <img src={logo30} width="30" height="30" alt={name + " logo"} />;
+  return (
+    <CurrentRouteContext.Provider value={props.route}>
+      <Navbar bg="light">
+        <ul className="navbar-nav mr-auto">
+          <Navbar.Brand>
+            {props.route === '/' ? logo :
+              <Link to="/">
+                {logo}
+              </Link>
+            }
+          </Navbar.Brand>
+          <Form inline>
+            <Button onClick={history.goBack} variant="outline-primary"><FontAwesomeIcon icon={faArrowLeft} /> Back</Button>
+          </Form>
+        </ul>
+        {props.route === '/search' ? null :
+          <ul className="navbar-nav flex-grow-1 justify-content-center">
             <Form inline>
-              <Button onClick={this.back} variant="outline-primary"><FontAwesomeIcon icon={faArrowLeft} /> Back</Button>
+              <Button as={Link} to="/search" variant="outline-primary"><FontAwesomeIcon icon={faSearch} /> Search</Button>
             </Form>
           </ul>
-          {this.props.route === '/search' ? null :
-            <ul className="navbar-nav flex-grow-1 justify-content-center">
-              <Form inline>
-                <Button as={Link} to="/search" variant="outline-primary"><FontAwesomeIcon icon={faSearch} /> Search</Button>
-              </Form>
-            </ul>
-          }
-          <ul className="navbar-nav">
-            <Form inline>
-              <Button as={Link} to="/settings" variant="outline-primary"><FontAwesomeIcon icon={faWrench} /> Settings</Button>
-            </Form>
-          </ul>
-        </Navbar>
-        <Container fluid>
-          <Row>
-            <Col lg="2" md="1" style={{backgroundImage: 'url(' + wallpaper + ')'}} />
-            <Col lg="8" md="10">
-              <div className="content">
-                {this.props.children}
-              </div>
-            </Col>
-            <Col lg="2" md="1" style={{backgroundImage: 'url(' + wallpaper + ')'}} />
-          </Row>
-        </Container>
-      </CurrentRouteContext.Provider>
-    );
-  }
+        }
+        <ul className="navbar-nav">
+          <Form inline>
+            <Button as={Link} to="/settings" variant="outline-primary"><FontAwesomeIcon icon={faWrench} /> Settings</Button>
+          </Form>
+        </ul>
+      </Navbar>
+      <Container fluid>
+        <Row>
+          <Col lg="2" md="1" style={{ backgroundImage: 'url(' + wallpaper + ')' }} />
+          <Col lg="8" md="10">
+            <div className="content">
+              {props.children}
+            </div>
+          </Col>
+          <Col lg="2" md="1" style={{ backgroundImage: 'url(' + wallpaper + ')' }} />
+        </Row>
+      </Container>
+    </CurrentRouteContext.Provider>
+  );
 }
-
-export default withRouter(Template);
